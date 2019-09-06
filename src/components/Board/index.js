585ae9f85c0ab06efd911417a7fef2660f8377a1
Daniel Klotz <dklotz@goc.realcore-it.de>
2019-09-06 10:31:26 +0200

    Added react context api to aux dnd

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import produce from 'immer';
 
 import List from '../List';
 import { loadLists } from '../../services/api';
 import { Container } from './styles';
+import BoardContext from './context';
 
-const lists = loadLists();
+const data = loadLists();
 
 export default function Board() {
+
+  const [lists, setLists] = useState(data);
+
+  function move(fromList, toList, from, to) {
+    setLists(produce(lists, draft => {
+      const dragged = draft[fromList].cards[from];
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+    }))
+  }
+
   return (
-    <Container>
-      {lists.map(list => <List key={list.title} data={list} />)}
-    </Container>
+    <BoardContext.Provider value={{lists, move}}>
+      <Container>
+        {lists.map((list, index) => <List key={list.title} data={list} index={index}/>)}
+      </Container>
+    </BoardContext.Provider>
   );
 }
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -35,7 +35,7 @@ export const Container = styled.div`
     border-radius: 0;
     background: transparent;
     box-shadow: none;
-    cursor: -webkit-grabbing;
+    cursor: grabbing;
 
     p, img, header {
       opacity: 0;
